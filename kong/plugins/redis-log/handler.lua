f33a8fb2c4e3ece9c77430a2f1b9ffd74e9a5acd
zhangguangjian <gzhang@heygears.com>
2017-09-02 00:43:48 +0800

    redis-log plugin can change to file log

--- kong/plugins/redis-log/handler.lua
+++ kong/plugins/redis-log/handler.lua
@@ -1,7 +1,10 @@
+
+local ffi = require "ffi"
 local BasePlugin = require "kong.plugins.base_plugin"
 local basic_serializer = require "kong.plugins.log-serializers.basic"
 local cjson = require "cjson"
 local redis = require "resty.redis"
+local system_constants = require "lua_system_constants"
 local pcall = pcall
 local ngx_req_get_post_args = ngx.req.get_post_args
 local json_decode = cjson.decode
@@ -13,10 +16,58 @@ local RedisLogHandler = BasePlugin:extend()
 -- we need to log log_data from response before transform
 RedisLogHandler.PRIORITY = 810
 
-local function log(premature, conf, message)
-    if premature then
-        return
+-- switch to the file log when the network of redis server is broken
+local O_CREAT = system_constants.O_CREAT()
+local O_WRONLY = system_constants.O_WRONLY()
+local O_APPEND = system_constants.O_APPEND()
+local S_IRUSR = system_constants.S_IRUSR()
+local S_IWUSR = system_constants.S_IWUSR()
+local S_IRGRP = system_constants.S_IRGRP()
+local S_IROTH = system_constants.S_IROTH()
+
+local oflags = bit.bor(O_WRONLY, O_CREAT, O_APPEND)
+local mode = bit.bor(S_IRUSR, S_IWUSR, S_IRGRP, S_IROTH)
+
+ffi.cdef[[
+int open(const char * filename, int flags, int mode);
+int write(int fd, const void * ptr, int numbytes);
+int close(int fd);
+char *strerror(int errnum);
+]]
+
+-- fd tracking utility functions
+local file_descriptors = {}
+
+local function log_to_file( conf, message )
+
+    local msg = json_encode(message) .. "\n"
+
+    local fd = file_descriptors[conf.path]
+
+    if fd and conf.reopen then
+        -- close fd, we do this here, to make sure a previously cached fd also
+        -- gets closed upon dynamic changes of the configuration
+        ffi.C.close(fd)
+        file_descriptors[conf.path] = nil
+        fd = nil
     end
+
+    if not fd then
+        fd = ffi.C.open(conf.path, oflags, mode)
+        if fd < 0 then
+            local errno = ffi.errno()
+            error("[redis-file-log] failed to open the file: " .. ffi.string(ffi.C.strerror(errno)))
+            return
+        else
+            file_descriptors[conf.path] = fd
+        end
+    end
+
+    ffi.C.write(fd, msg, #msg)
+end
+
+-- log to redis server
+local function log_to_redis( conf, message )
 
     local ok, err
     local host = conf.host
@@ -30,7 +81,7 @@ local function log(premature, conf, message)
     red:set_timeout(timeout)
     ok, err = red:connect(host, port)
     if not ok then
-        ngx.log(ngx.ERR, "[redis-log] failed to connect to " .. host .. ":" .. tostring(port) .. ": ", err)
+        error("[redis-log] failed to connect to " .. host .. ":" .. tostring(port) .. ": " .. err)
         return
     end
 
@@ -45,33 +96,76 @@ local function log(premature, conf, message)
         if password and password ~= "" then
             ok, err = red:auth(password)
             if not ok then
-                ngx.log(ngx.ERR, "[redis-log] failed to connect to " .. host .. ":" .. tostring(port) .. ": ", err)
+                error("[redis-log] failed to connect to " .. host .. ":" .. tostring(port) .. ": " .. err)
                 return
             end
         end
     elseif err then
-        ngx.log(ngx.ERR, "[redis-log] failed to connect to " .. host .. ":" .. tostring(port) .. ": ", err)
+        error("[redis-log] failed to connect to " .. host .. ":" .. tostring(port) .. ": " .. err)
         return
     end
 
     if database ~= nil and database > 0 then
         ok, err = red:select(database)
         if not ok then
-            ngx.log(ngx.ERR, "[redis-log] failed to change Redis database from " .. host .. ":" .. tostring(port) .. ": ", err)
+            error("[redis-log] failed to change Redis database from " .. host .. ":" .. tostring(port) .. ": " .. err)
             return
         end
     end
 
     ok, err = red:rpush(key, json_encode(message) .. "\r\n")
     if not ok then
-        ngx.log(ngx.ERR, "[redis-log] failed to send data to " .. host .. ":" .. tostring(port) .. ": ", err)
+        error("[redis-log] failed to send data to " .. host .. ":" .. tostring(port) .. ": " .. err)
+        return
     end
 
     -- pool size is 100, and the maximum idle time is set to 10 seconds
     ok, err = red:set_keepalive(10000, 100)
     if not ok then
-        ngx.log(ngx.ERR, "[redis-log] failed to keepalive to " .. host .. ":" .. tostring(port) .. ": ", err)
+        error("[redis-log] failed to keepalive to " .. host .. ":" .. tostring(port) .. ": " .. err)
         return
+    end
+end
+
+--  error switch
+local is_redis_error = false
+
+local function check_redis()
+    is_redis_error = false
+end
+
+-- Log to redis or log to a file when redis server is broken. 
+-- Function used as callback from an nginx timer.
+-- @param `premature` see OpenResty `ngx.timer.at()`
+-- the premature argument takes a boolean value indicating whether it is a premature timer expiration or not
+-- @param `conf`     Configuration table, holds http endpoint details
+-- @param `message`  Message to be logged
+local function log(premature, conf, message)
+    if is_redis_error then
+        -- change to file log
+        local ok, error_msg = pcall(log_to_file, conf, message)
+        if not ok then
+            ngx.log(ngx.ERR, error_msg)
+        end
+        return
+    end
+
+    local ok, error_msg = pcall(log_to_redis, conf, message)
+    if not ok then
+        is_redis_error = true
+        ngx.log(ngx.ERR, error_msg)
+
+        -- change to file log
+        local ok, error_msg = pcall(log_to_file, conf, message)
+        if not ok then
+            ngx.log(ngx.ERR, error_msg)
+        end
+
+        -- regular check redis
+        local ok, err = ngx.timer.at(conf.broken_timeout, check_redis)
+        if not ok then
+            ngx.log(ngx.ERR, "[redis-log] failed to create check_redis timer: ", err)
+        end
     end
 end
 
@@ -103,7 +197,7 @@ function RedisLogHandler:log(conf)
     end
   
     -- log JSON date
-    local content_type = message.response.headers["Content-Type"];
+    local content_type = message.response.headers["Content-Type"]
     if content_type ~= nil and string.lower(content_type) == "application/json" and ngx.ctx.resp_body ~= nil then
         -- solve the chinese problem, because the data is serialized
         local ok, ret = pcall(json_decode, ngx.ctx.resp_body)
--- kong/plugins/redis-log/schema.lua
+++ kong/plugins/redis-log/schema.lua
@@ -1,3 +1,18 @@
+local pl_file = require "pl.file"
+local pl_path = require "pl.path"
+
+local function validate_file(value)
+  -- create file in case it doesn't exist
+  if not pl_path.exists(value) then
+    local ok, err = pl_file.write(value, "")
+    if not ok then
+      return false, string.format("Cannot create file: %s", err)
+    end
+  end
+
+  return true
+end
+
 return {
   fields = {
     host = { required = true, type = "string" },
@@ -5,6 +20,9 @@ return {
     database = { default = 0, type = "number" },
     password = { required = true, type = "string" },
     key = { required = true, type = "string" },
-    timeout = { default = 2000, type = "number" }
+    timeout = { default = 2000, type = "number" },
+    path = { required = true, type = "string", func = validate_file },
+    reopen = { type = "boolean", default = false },
+    broken_timeout = {default = 5, type = "number"}
   }
 }
